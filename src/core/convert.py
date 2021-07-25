a3208059d92b6269f04e3b63b4929fa001addb01
Matin Najafi <matin.najafi@gmail.com>
2021-07-25 14:49:52 +0430

    Add null filter for convert functions

--- src/core/convert.py
+++ src/core/convert.py
@@ -1,9 +1,13 @@
 def tomb(text):
+    if not text:
+        return
     base = [ord(i) for i in text]
     base2 = [j*(i+1) for i, j in enumerate(base)]
     return ' '.join([str(i) for i in base2])
 
 def totext(text):
+    if not text:
+        return
     text = text.split(' ')
     base = [int(int(j)/(index+1)) for index, j in enumerate(text)]
     return ''.join([chr(i) for i in base])
