fdfa9f3be19664b343c853b011f54f73b58c11e5
ArtaZDX <arta.moghaddasi@gmail.com>
2021-07-31 06:10:03 -0700

    update error handling in shell

--- src/shell.py
+++ src/shell.py
@@ -14,10 +14,14 @@ class Shell(parser.Parser):
                 sys.exit(1)
             run = self.run_line(__input)
             if type(run) != type:
-                if(run == errors.MBSyntaxError):
-                    print('[Syntax Error]')
-                    continue
-                print()
+                errors_list = {cl:eval('errors.'+cl) for cl in dir(errors)}
+                print(errors_list)
+                if run in [j for i,j in errors_list.items()]:
+                    print('['+str(
+                        [i for i,j in errors_list.items()][list(errors_list.values()).index(run)]
+                    )+'] on line '+str(self.data.index(__input)+1))
+                    if self.auto_run:
+                        sys.exit(1)
 
 try:
     shell = Shell()
